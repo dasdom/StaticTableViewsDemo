c7351d7e86a47e5113c0d907fd66a656962408a2
Dominik Hauser <dom@dasdom.de>
2021-12-10 22:04:21 +0100

    Initial Commit

---
+++ StaticTableViewsDemo/ViewController.swift
@@ -0,0 +1,16 @@
+//  Created by Dominik Hauser on 10.12.21.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
